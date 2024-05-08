f668449a41b68dee7e4dbfdf29a9db5bc6bc9175
Magnus Hagander <magnus@hagander.net>
2024-05-08 17:39:22 +0200

    Update url matching for django 4.2

--- pgcommitfest/urls.py
+++ pgcommitfest/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.urls import re_path
 from django.contrib import admin
 
 import pgcommitfest.commitfest.views as views
@@ -14,44 +14,44 @@ admin.autodiscover()
 
 
 urlpatterns = [
-    url(r'^$', views.home),
-    url(r'^activity(?P<rss>\.rss)?/', views.activity),
-    url(r'^(\d+)/$', views.commitfest),
-    url(r'^(open|inprogress)/$', views.redir),
-    url(r'^(?P<cfid>\d+)/activity(?P<rss>\.rss)?/$', views.activity),
-    url(r'^(\d+)/(\d+)/$', views.patch),
-    url(r'^(\d+)/(\d+)/edit/$', views.patchform),
-    url(r'^(\d+)/new/$', views.newpatch),
-    url(r'^(\d+)/(\d+)/status/(review|author|committer)/$', views.status),
-    url(r'^(\d+)/(\d+)/close/(reject|withdrawn|feedback|committed|next)/$', views.close),
-    url(r'^(\d+)/(\d+)/reviewer/(become|remove)/$', views.reviewer),
-    url(r'^(\d+)/(\d+)/committer/(become|remove)/$', views.committer),
-    url(r'^(\d+)/(\d+)/(un)?subscribe/$', views.subscribe),
-    url(r'^(\d+)/(\d+)/(comment|review)/', views.comment),
-    url(r'^(\d+)/send_email/$', views.send_email),
-    url(r'^(\d+)/\d+/send_email/$', views.send_email),
-    url(r'^(\d+)/reports/authorstats/$', reports.authorstats),
-    url(r'^search/$', views.global_search),
-    url(r'^ajax/(\w+)/$', ajax.main),
-    url(r'^lookups/user/$', lookups.userlookup),
-    url(r'^thread_notify/$', views.thread_notify),
+    re_path(r'^$', views.home),
+    re_path(r'^activity(?P<rss>\.rss)?/', views.activity),
+    re_path(r'^(\d+)/$', views.commitfest),
+    re_path(r'^(open|inprogress)/$', views.redir),
+    re_path(r'^(?P<cfid>\d+)/activity(?P<rss>\.rss)?/$', views.activity),
+    re_path(r'^(\d+)/(\d+)/$', views.patch),
+    re_path(r'^(\d+)/(\d+)/edit/$', views.patchform),
+    re_path(r'^(\d+)/new/$', views.newpatch),
+    re_path(r'^(\d+)/(\d+)/status/(review|author|committer)/$', views.status),
+    re_path(r'^(\d+)/(\d+)/close/(reject|withdrawn|feedback|committed|next)/$', views.close),
+    re_path(r'^(\d+)/(\d+)/reviewer/(become|remove)/$', views.reviewer),
+    re_path(r'^(\d+)/(\d+)/committer/(become|remove)/$', views.committer),
+    re_path(r'^(\d+)/(\d+)/(un)?subscribe/$', views.subscribe),
+    re_path(r'^(\d+)/(\d+)/(comment|review)/', views.comment),
+    re_path(r'^(\d+)/send_email/$', views.send_email),
+    re_path(r'^(\d+)/\d+/send_email/$', views.send_email),
+    re_path(r'^(\d+)/reports/authorstats/$', reports.authorstats),
+    re_path(r'^search/$', views.global_search),
+    re_path(r'^ajax/(\w+)/$', ajax.main),
+    re_path(r'^lookups/user/$', lookups.userlookup),
+    re_path(r'^thread_notify/$', views.thread_notify),
 
     # Auth system integration
-    url(r'^(?:account/)?login/?$', pgcommitfest.auth.login),
-    url(r'^(?:account/)?logout/?$', pgcommitfest.auth.logout),
-    url(r'^auth_receive/$', pgcommitfest.auth.auth_receive),
-    url(r'^auth_api/$', pgcommitfest.auth.auth_api),
+    re_path(r'^(?:account/)?login/?$', pgcommitfest.auth.login),
+    re_path(r'^(?:account/)?logout/?$', pgcommitfest.auth.logout),
+    re_path(r'^auth_receive/$', pgcommitfest.auth.auth_receive),
+    re_path(r'^auth_api/$', pgcommitfest.auth.auth_api),
 
     # Account management
-    url(r'^account/profile/$', pgcommitfest.userprofile.views.userprofile),
+    re_path(r'^account/profile/$', pgcommitfest.userprofile.views.userprofile),
 
     # Examples:
-    # url(r'^$', 'pgpgcommitfest.commitfest.views.home', name='home),
-    # url(r'^pgcommitfest/', include('pgcommitfest.foo.urls)),
+    # re_path(r'^$', 'pgpgcommitfest.commitfest.views.home', name='home),
+    # re_path(r'^pgcommitfest/', include('pgcommitfest.foo.urls)),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls)),
+    # re_path(r'^admin/doc/', include('django.contrib.admindocs.urls)),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', admin.site.urls),
+    re_path(r'^admin/', admin.site.urls),
 ]
