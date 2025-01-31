8d3802aec5690d37f922b68ec95dfadf034c16d2
Jelte Fennema-Nio <postgres@jeltef.nl>
2025-01-31 15:30:36 +0100

    Implement cache busting for our own static files (#23)
    Browsers cache static files. That's obviously good. Currently it results
    in our we change files server side, users don't always see them.
    
    This generates a GET parameter that's unique per startup of the python
    process to bust the cache of the client, so that it pulls in possibly
    updated JS/CSS files. Obviously this is a bit more often than necessary,
    but caching should still work most of the time and our custom css and js
    files are tiny anyway. At least the changes that are made will now be
    reflected to our users.

--- pgcommitfest/commitfest/templatetags/commitfest.py
+++ pgcommitfest/commitfest/templatetags/commitfest.py
@@ -1,5 +1,6 @@
 from django.template.defaultfilters import stringfilter
 from django import template
+from uuid import uuid4
 
 from pgcommitfest.commitfest.models import PatchOnCommitFest
 
@@ -41,6 +42,18 @@ def alertmap(value):
         return 'alert-success'
     else:
         return 'alert-info'
+
+
+# Generate a GET parameter that's unique per startup of the python process to
+# bust the cache of the client, so that it pulls in possibly updated JS/CSS
+# files.
+STATIC_FILE_PARAM = f"v={uuid4()}"
+
+
+# This GET parameter should be added to every one of our static files.
+@register.simple_tag
+def static_file_param():
+    return STATIC_FILE_PARAM
 
 
 @register.filter(name='hidemail')
