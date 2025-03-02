postgres/pgcommitfest

commit 2e6f028754ea5a4fe7488999eae8e18035a5959b

Revamp the homepage to show you patches you might be interested in (#51)
The homepage was not showing a lot of useful information. It had a list
of mostly ancient commitfests, a search box, and a few links to more
useful pages. This revamps the homepage with an initial version of a
personalized dashboard. The dashboard shows patches that the user is
subscribed to, either because they are marked as an author or a
reviewer. We then try to group and sort these patches in an attempt to
let the user easily find the patches that require their attention.

Many things can likely be tweaked/improved. I think this first version
is already a huge improvement over the status quo, so I think it's worth
committing deploying this mostly as is and then iterating on it based on
feedback.

This is what it looks like:
![screenshot of
homepage](https://github.com/user-attachments/assets/6f464597-30f9-4f4b-9b98-17ac90b64b60)

Fixes #36

pgcommitfest/commitfest/views.py
@@ -13,6 +13,7 @@ from django.http import (
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.csrf import csrf_exempt
 
+import collections
 import hmac
 import json
 import urllib
@@ -44,22 +45,56 @@ from .models import (
 
 
 def home(request):
-    commitfests = list(CommitFest.objects.all())
-    opencf = next((c for c in commitfests if c.status == CommitFest.STATUS_OPEN), None)
-    inprogresscf = next(
-        (c for c in commitfests if c.status == CommitFest.STATUS_INPROGRESS), None
-    )
+    cfs = list(CommitFest.objects.filter(status=CommitFest.STATUS_INPROGRESS))
+    if len(cfs) == 0:
+        cfs = list(CommitFest.objects.filter(status=CommitFest.STATUS_OPEN))
+
+    if len(cfs) > 0:
+        cf = cfs[0]
+    else:
+        cf = None
+
+    # Generates a fairly expensive query, which we shouldn't do unless
+    # the user is logged in. XXX: Figure out how to avoid doing that..
+    form = CommitFestFilterForm(None, request.GET)
+
+    if request.user.is_authenticated:
+        patch_list = patchlist(request, cf, personalized=True)
+    else:
+        patch_list = patchlist(request, cf)
+
+    if patch_list.redirect:
+        return HttpResponseRedirect("/")
 
     return render(
         request,
         "home.html",
         {
-            "commitfests": commitfests,
-            "opencf": opencf,
-            "inprogresscf": inprogresscf,
-            "title": "Commitfests",
+            "form": form,
+            "title": None,
+            "patches": patch_list.patches,
+            "statussummary": "",
+            "has_filter": patch_list.has_filter,
+            "grouping": patch_list.sortkey == 0,
+            "sortkey": patch_list.sortkey,
+            "openpatchids": [p["id"] for p in patch_list.patches if p["is_open"]],
             "header_activity": "Activity log",
             "header_activity_link": "/activity/",
+        },
+    )
+
+
+def archive(request):
+    commitfests = list(CommitFest.objects.all())
+
+    return render(
+        request,
+        "archive.html",
+        {
+            "commitfests": commitfests,
+            "title": "Commitfests",
+            "header_activity": "Activity log",
+            "header_activity_link": "activity/",
         },
     )
 
@@ -150,13 +185,16 @@ def redir(request, what, end):
     return HttpResponseRedirect(f"/{cfs[0].id}/{end}{query_string}")
 
 
-def commitfest(request, cfid):
-    # Find ourselves
-    cf = get_object_or_404(CommitFest, pk=cfid)
+PatchList = collections.namedtuple(
+    "PatchList", ["patches", "has_filter", "sortkey", "redirect"]
+)
 
+
+def patchlist(request, cf, personalized=False):
     # Build a dynamic filter based on the filtering options entered
     whereclauses = []
     whereparams = {}
+
     if request.GET.get("status", "-1") != "-1":
         try:
             whereparams["status"] = int(request.GET["status"])
@@ -232,68 +270,159 @@ def commitfest(request, cfid):
 
     has_filter = len(whereclauses) > 0
 
-    # Figure out custom ordering
-    if request.GET.get("sortkey", "") != "":
-        try:
-            sortkey = int(request.GET["sortkey"])
-        except ValueError:
-            sortkey = 0
+    if personalized:
+        whereclauses.append("""
+            EXISTS (
+                SELECT 1 FROM commitfest_patch_reviewers cpr WHERE cpr.patch_id=p.id AND cpr.user_id=%(self)s
+            ) OR EXISTS (
+                SELECT 1 FROM commitfest_patch_authors cpa WHERE cpa.patch_id=p.id AND cpa.user_id=%(self)s
+            ) OR p.committer_id=%(self)s""")
+        whereparams["self"] = request.user.id
 
-        if sortkey == 2:
-            orderby_str = "lastmail, created"
-        elif sortkey == -2:
-            orderby_str = "lastmail DESC, created DESC"
-        elif sortkey == 3:
-            orderby_str = "num_cfs DESC, modified, created"
-        elif sortkey == -3:
-            orderby_str = "num_cfs ASC, modified DESC, created DESC"
-        elif sortkey == 4:
-            orderby_str = "p.id"
-        elif sortkey == -4:
-            orderby_str = "p.id DESC"
-        elif sortkey == 5:
-            orderby_str = "p.name, created"
-        elif sortkey == -5:
-            orderby_str = "p.name DESC, created DESC"
-        elif sortkey == 6:
-            orderby_str = (
-                "branch.all_additions + branch.all_deletions NULLS LAST, created"
-            )
-        elif sortkey == -6:
-            orderby_str = "branch.all_additions + branch.all_deletions DESC NULLS LAST, created DESC"
-        elif sortkey == 7:
-            orderby_str = "branch.failing_since DESC NULLS FIRST, branch.created DESC"
-        elif sortkey == -7:
-            orderby_str = "branch.failing_since NULLS LAST, branch.created"
-        else:
-            orderby_str = "p.id"
-            sortkey = 0
+        whereclauses.append("poc.status=ANY(%(openstatuses)s)")
     else:
-        orderby_str = "topic, created"
+        whereclauses.append("poc.commitfest_id=%(cid)s")
+
+    if personalized:
+        # For now we can just order by these names in descending order, because
+        # they are crafted such that they alphabetically sort in the intended
+        # order.
+        columns_str = """
+            CASE WHEN
+                EXISTS (
+                    SELECT 1 FROM commitfest_patch_authors cpa WHERE cpa.patch_id=p.id AND cpa.user_id=%(self)s
+                ) AND (
+                    poc.commitfest_id < %(cid)s
+                )
+            THEN 'Your still open patches in a closed commitfest (you should move or close these)'
+            WHEN
+                EXISTS (
+                    SELECT 1 FROM commitfest_patch_authors cpa WHERE cpa.patch_id=p.id AND cpa.user_id=%(self)s
+                ) AND (
+                    poc.status=%(needs_author)s
+                    OR branch.needs_rebase_since IS NOT NULL
+                    OR branch.failing_since + interval '4 days' < now()
+                    OR (%(is_committer)s AND poc.status=%(needs_committer)s)
+                )
+            THEN 'Your patches that need changes from you'
+            WHEN
+                NOT EXISTS (
+                    SELECT 1 FROM commitfest_patch_authors cpa WHERE cpa.patch_id=p.id AND cpa.user_id=%(self)s
+                ) AND (
+                    poc.status=ANY(%(review_statuses)s)
+                )
+            THEN 'Patches that are ready for your review'
+            ELSE 'Blocked on others'
+            END AS topic,
+            cf.id AS cf_id,
+            cf.name AS cf_name,
+            cf.status AS cf_status,
+        """
+        whereparams["needs_author"] = PatchOnCommitFest.STATUS_AUTHOR
+        whereparams["needs_committer"] = PatchOnCommitFest.STATUS_COMMITTER
+        is_committer = bool(Committer.objects.filter(user=request.user, active=True))
+        whereparams["is_committer"] = is_committer
+
+        if is_committer:
+            whereparams["review_statuses"] = [
+                PatchOnCommitFest.STATUS_REVIEW,
+                PatchOnCommitFest.STATUS_COMMITTER,
+            ]
+        else:
+            whereparams["review_statuses"] = [
+                PatchOnCommitFest.STATUS_REVIEW,
+            ]
+        joins_str = "INNER JOIN commitfest_commitfest cf ON poc.commitfest_id=cf.id"
+        groupby_str = "cf.id,"
+    else:
+        columns_str = "t.topic as topic,"
+        joins_str = ""
+        groupby_str = ""
+
+    # Figure out custom ordering
+    try:
+        sortkey = int(request.GET.get("sortkey", "0"))
+    except ValueError:
+        sortkey = 0
+
+    if sortkey == 2:
+        orderby_str = "lastmail, created"
+    elif sortkey == -2:
+        orderby_str = "lastmail DESC, created DESC"
+    elif sortkey == 3:
+        orderby_str = "num_cfs DESC, modified, created"
+    elif sortkey == -3:
+        orderby_str = "num_cfs ASC, modified DESC, created DESC"
+    elif sortkey == 4:
+        orderby_str = "p.id"
+    elif sortkey == -4:
+        orderby_str = "p.id DESC"
+    elif sortkey == 5:
+        orderby_str = "p.name, created"
+    elif sortkey == -5:
+        orderby_str = "p.name DESC, created DESC"
+    elif sortkey == 6:
+        orderby_str = "branch.all_additions + branch.all_deletions NULLS LAST, created"
+    elif sortkey == -6:
+        orderby_str = (
+            "branch.all_additions + branch.all_deletions DESC NULLS LAST, created DESC"
+        )
+    elif sortkey == 7:
+        orderby_str = "branch.failing_since DESC NULLS FIRST, branch.created DESC"
+    elif sortkey == -7:
+        orderby_str = "branch.failing_since NULLS LAST, branch.created"
+    elif sortkey == 8:
+        orderby_str = "poc.commitfest_id, lastmail DESC"
+    elif sortkey == -8:
+        orderby_str = "poc.commitfest_id DESC, lastmail"
+    else:
+        if personalized:
+            # First we sort by topic, to have the grouping work.
+            # Then we show non-failing patches first, and the ones that are
+            # shortest failing we show first. We consider patches in a closed
+            # commitfest, as if they are failing since that commitfest was
+            # closed.
+            # Then we sort by start date of the CF, to show entries in the "In
+            # progress" commitfest before ones in the "Open" commitfest.
+            # And then to break ties, we put ones with the most recent email at
+            # the top.
+            orderby_str = """topic DESC,
+                COALESCE(
+                    branch.failing_since,
+                    CASE WHEN cf.status = %(cf_closed_status)s
+                    THEN enddate ELSE NULL END
+                ) DESC NULLS FIRST,
+                cf.startdate,
+                lastmail DESC"""
+            whereparams["cf_closed_status"] = CommitFest.STATUS_CLOSED
+        else:
+            orderby_str = "topic, created"
         sortkey = 0
 
     if not has_filter and sortkey == 0 and request.GET:
         # Redirect to get rid of the ugly url
-        return HttpResponseRedirect("/%s/" % cf.id)
+        return PatchList(patches=[], has_filter=False, sortkey=0, redirect=True)
 
     if whereclauses:
-        where_str = "AND ({0})".format(" AND ".join(whereclauses))
+        where_str = "({0})".format(") AND (".join(whereclauses))
     else:
-        where_str = ""
+        where_str = "true"
     params = {
-        "cid": cf.id,
         "openstatuses": PatchOnCommitFest.OPEN_STATUSES,
+        "cid": cf.id,
     }
     params.update(whereparams)
 
     # Let's not overload the poor django ORM
     curs = connection.cursor()
     curs.execute(
-        """SELECT p.id, p.name, poc.status, v.version AS targetversion, p.created, p.modified, p.lastmail, committer.first_name || ' ' || committer.last_name || ' (' || committer.username || ')' AS committer, t.topic,
+        f"""SELECT p.id, p.name, poc.status, v.version AS targetversion, p.created, p.modified, p.lastmail, committer.first_name || ' ' || committer.last_name || ' (' || committer.username || ')' AS committer,
+        {columns_str}
 (poc.status=ANY(%(openstatuses)s)) AS is_open,
 (SELECT string_agg(first_name || ' ' || last_name || ' (' || username || ')', ', ') FROM auth_user INNER JOIN commitfest_patch_authors cpa ON cpa.user_id=auth_user.id WHERE cpa.patch_id=p.id) AS author_names,
 (SELECT string_agg(first_name || ' ' || last_name || ' (' || username || ')', ', ') FROM auth_user INNER JOIN commitfest_patch_reviewers cpr ON cpr.user_id=auth_user.id WHERE cpr.patch_id=p.id) AS reviewer_names,
 (SELECT count(1) FROM commitfest_patchoncommitfest pcf WHERE pcf.patch_id=p.id) AS num_cfs,
+
 branch.needs_rebase_since,
 branch.failing_since,
 (
@@ -319,17 +448,33 @@ branch.failing_since,
 FROM commitfest_patch p
 INNER JOIN commitfest_patchoncommitfest poc ON poc.patch_id=p.id
 INNER JOIN commitfest_topic t ON t.id=p.topic_id
+{joins_str}
 LEFT JOIN auth_user committer ON committer.id=p.committer_id
 LEFT JOIN commitfest_targetversion v ON p.targetversion_id=v.id
 LEFT JOIN commitfest_cfbotbranch branch ON branch.patch_id=p.id
-WHERE poc.commitfest_id=%(cid)s {0}
-GROUP BY p.id, poc.id, committer.id, t.id, v.version, branch.patch_id
-ORDER BY is_open DESC, {1}""".format(where_str, orderby_str),
+WHERE {where_str}
+GROUP BY p.id, poc.id, {groupby_str} committer.id, t.id, v.version, branch.patch_id
+ORDER BY is_open DESC, {orderby_str}""",
         params,
     )
     patches = [
         dict(zip([col[0] for col in curs.description], row)) for row in curs.fetchall()
     ]
+    return PatchList(
+        patches=patches,
+        sortkey=sortkey,
+        has_filter=has_filter,
+        redirect=False,
+    )
+
+
+def commitfest(request, cfid):
+    # Find ourselves
+    cf = get_object_or_404(CommitFest, pk=cfid)
+
+    patch_list = patchlist(request, cf)
+    if patch_list.redirect:
+        return HttpResponseRedirect(f"/{cf.id}/")
 
     # Generate patch status summary.
     curs = connection.cursor()
@@ -352,13 +497,13 @@ ORDER BY is_open DESC, {1}""".format(where_str, orderby_str),
         {
             "cf": cf,
             "form": form,
-            "patches": patches,
+            "patches": patch_list.patches,
             "statussummary": statussummary,
-            "has_filter": has_filter,
+            "has_filter": patch_list.has_filter,
             "title": cf.title,
-            "grouping": sortkey == 0,
-            "sortkey": sortkey,
-            "openpatchids": [p["id"] for p in patches if p["is_open"]],
+            "grouping": patch_list.sortkey == 0,
+            "sortkey": patch_list.sortkey,
+            "openpatchids": [p["id"] for p in patch_list.patches if p["is_open"]],
             "header_activity": "Activity log",
             "header_activity_link": "activity/",
         },

media/commitfest/css/commitfest.css
@@ -87,3 +87,7 @@ div.form-group div.controls input.threadpick-input {
     font-weight: bold;
     color: red;
 }
+
+.search-bar {
+    display: inline-block;
+}

pgcommitfest/commitfest/forms.py
@@ -26,7 +26,10 @@ class CommitFestFilterForm(forms.Form):
         c = [(-1, "* All")] + list(PatchOnCommitFest._STATUS_CHOICES)
         self.fields["status"] = forms.ChoiceField(choices=c, required=False)
 
-        q = Q(patch_author__commitfests=cf) | Q(patch_reviewer__commitfests=cf)
+        if cf:
+            q = Q(patch_author__commitfests=cf) | Q(patch_reviewer__commitfests=cf)
+        else:
+            q = Q()
         userchoices = [(-1, "* All"), (-2, "* None"), (-3, "* Yourself")] + [
             (u.id, "%s %s (%s)" % (u.first_name, u.last_name, u.username))
             for u in User.objects.filter(q)

pgcommitfest/commitfest/models.py
@@ -44,6 +44,12 @@ class CommitFest(models.Model):
         (STATUS_INPROGRESS, "In Progress"),
         (STATUS_CLOSED, "Closed"),
     )
+    _STATUS_LABELS = (
+        (STATUS_FUTURE, "default"),
+        (STATUS_OPEN, "info"),
+        (STATUS_INPROGRESS, "success"),
+        (STATUS_CLOSED, "danger"),
+    )
     name = models.CharField(max_length=100, blank=False, null=False, unique=True)
     status = models.IntegerField(
         null=False, blank=False, default=1, choices=_STATUS_CHOICES

pgcommitfest/commitfest/templates/archive.html
@@ -1,0 +1,9 @@
+{%extends "base.html"%}
+{%block contents%}
+ <ul>
+  {%for c in commitfests%}
+   <li><a href="/{{c.id}}/">{{c}}</a> ({{c.statusstring}}{%if c.startdate%} - {{c.periodstring}}{%endif%})</li>
+  {%endfor%}
+ </ul>
+{%endblock%}
+

pgcommitfest/commitfest/templates/base.html
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
  <head>
-  <title>{{title}}</title>
+  <title>{{title|default:'Commitfest' }}</title>
   <link rel="stylesheet" href="/media/commitfest/css/jquery-ui.css" type="text/css">
   <link rel="stylesheet" href="/media/commitfest/css/bootstrap.css" />
   <link rel="stylesheet" href="/media/commitfest/css/bootstrap-theme.min.css" />
@@ -29,7 +29,9 @@
     {%if header_activity%} <li class="pull-right active"><a href="{{header_activity_link}}">{{header_activity}}</a></li>{%endif%}
    </ul>
 
-   <h1>{{title}}</h1>
+   {%if title %}
+    <h1>{{title}}</h1>
+   {%endif%}
 
    {%if messages%}
     {%for m in messages%}

pgcommitfest/commitfest/templates/home.html
@@ -1,31 +1,135 @@
 {%extends "base.html"%}
+{%load commitfest %}
 {%block contents%}
- <p>
-  {%if inprogresscf%}A commitfest is currently in progress: <a href="/{{inprogresscf.id}}/">{{inprogresscf}}</a>.{%endif%}
- </p>
- <p>
-  Useful links that you can use and bookmark:
- </p>
- <ul>
-  <li><a href="/current/">All patches in the current commitfest</a></li>
-  <li><a href="/open/">All patches in the open commitfest</a></li>
-  <li><a href="/open/new/">Create a new commitfest entry</a></li>
-  <li><a href="/current/?author=-3">Your entries in the current commitfest</a></li>
-  <li><a href="/open/?author=-3">Your entries in the open commitfest</a></li>
-  <li><a href="/current/?reviewer=-3">Entries that you are reviewing in current commitfest</a></li>
- </ul>
- <h3>Commands</h3>
- <form method="GET" action="/search/" class="form-inline">
+ <a class="btn btn-default" href="/open/new/">New patch</a>
+ <a class="btn btn-default" href="/current/">Current commitfest</a></li>
+ <a class="btn btn-default" href="/open/">Open commitfest</a></li>
+ <a class="btn btn-default" href="/archive/">Archive</a></li>
+ <button type="button" class="btn btn-default{%if has_filter%} active{%endif%}" id="filterButton" onClick="togglePatchFilterButton('filterButton', 'collapseFilters')">Search/filter</button>
+ <form method="GET" action="/search/" class="form-inline search-bar pull-right">
   <div class="form-group">
-   <input type="text" class="form-control" id="searchterm" name="searchterm" placeholder="Global search">
+   <input type="text" class="form-control" id="searchterm" name="searchterm" placeholder="Keyword or Message-ID">
   </div>
   <button type="submit" class="btn btn-default">Search</button>
  </form>
- <h3>List of commitfests</h3>
- <ul>
-  {%for c in commitfests%}
-   <li><a href="/{{c.id}}/">{{c}}</a> ({{c.statusstring}}{%if c.startdate%} - {{c.periodstring}}{%endif%})</li>
-  {%endfor%}
- </ul>
- <br/>
+ <div id="collapseFilters" class="collapse {%if has_filter%}in{%endif%}">
+  <form id="filterform" method="GET" action="." style="margin-bottom: 0px">
+   <table class="table table-condensed" style="margin-bottom: 0px">
+    <thead>
+     <tr>
+      {%for f in form%}
+       {%if not f.is_hidden%}
+        <td>{{f.label}}</td>
+       {%else%}
+        <td></td>
+       {%endif%}
+      {%endfor%}
+	    {%comment%} Add one extra for buttons {%endcomment%}
+      <td></td>
+     </tr>
+    </thead>
+    <tbody>
+     <tr>
+      {%for f in form%}
+       <td>{{f|field_class:"form-control"}}</td>
+      {%endfor%}
+      <td>
+       <input type="submit" class="btn btn-default" value="Filter">
+       <a class="btn btn-default" href=".">Clear</a>
+      </td>
+     </tr>
+    </tbody>
+   </table>
+  </form>
+ </div>
+ {%for p in patches %}
+  {%ifchanged p.is_open%}
+   {%if not forloop.first%}
+    </tbody>
+    </table>
+   {%endif%}
+   <h3>{{user.is_authenticated|yesno:"Your personal dashboard,Patches in the current commitfest"}}</h3>
+   <table class="table table-striped table-bordered table-hover table-condensed">
+    <thead>
+     <tr>
+      <th><a href="#" style="color:#333333;" onclick="return sortpatches(5);">Patch</a>{%if sortkey == 5%}<div style="float:right;"><i class="glyphicon glyphicon-arrow-down"></i></div>{%elif sortkey == -5%}<div style="float:right;"><i class="glyphicon glyphicon-arrow-up"></i></div>{%endif%}</th>
+      <th><a href="#" style="color:#333333;" onclick="return sortpatches(4);">ID</a>{%if sortkey == 4%}<div style="float:right;"><i class="glyphicon glyphicon-arrow-down"></i></div>{%elif sortkey == -4%}<div style="float:right;"><i class="glyphicon glyphicon-arrow-up"></i></div>{%endif%}</th>
+      {%if user.is_authenticated %}
+       <th><a href="#" style="color:#333333;" onclick="return sortpatches(8);">CF</a>{%if sortkey == 8%}<div style="float:right;"><i class="glyphicon glyphicon-arrow-down"></i></div>{%elif sortkey == -8%}<div style="float:right;"><i class="glyphicon glyphicon-arrow-up"></i></div>{%endif%}</th>
+      {%endif%}
+      <th>Status</th>
+      <th>Ver</th>
+      <th><a href="#" style="color:#333333;" onclick="return sortpatches(7);">CI status</a>{%if sortkey == 7%}<div style="float:right;"><i class="glyphicon glyphicon-arrow-down"></i></div>{%elif sortkey == -7%}<div style="float:right;"><i class="glyphicon glyphicon-arrow-up"></i></div>{%endif%}</th>
+      <th><a href="#" style="color:#333333;" onclick="return sortpatches(6);">Stats</a>{%if sortkey == 6%}<div style="float:right;"><i class="glyphicon glyphicon-arrow-down"></i></div>{%elif sortkey == -6%}<div style="float:right;"><i class="glyphicon glyphicon-arrow-up"></i></div>{%endif%}</th>
+      <th>Author</th>
+      <th>Reviewers</th>
+      <th>Committer</th>
+      <th><a href="#" style="color:#333333;" onclick="return sortpatches(3);">Num cfs</a>{%if sortkey == 3%}<div style="float:right;"><i class="glyphicon glyphicon-arrow-down"></i></div>{%elif sortkey == -3%}<div style="float:right;"><i class="glyphicon glyphicon-arrow-up"></i></div>{%endif%}</th>
+      <th><a href="#" style="color:#333333;" onclick="return sortpatches(2);">Latest mail</a>{%if sortkey == 2%}<div style="float:right;"><i class="glyphicon glyphicon-arrow-down"></i></div>{%elif sortkey == -2%}<div style="float:right;"><i class="glyphicon glyphicon-arrow-up"></i></div>{%endif%}</th>
+      {%if user.is_staff%}
+       <th>Select</th>
+      {%endif%}
+     </tr>
+    </thead>
+    <tbody>
+  {%endifchanged%}
+
+  {%if grouping%}
+   {%ifchanged p.topic%}
+    <tr><th colspan="{%if user.is_staff%}13{%elif user.is_authenticated %}12{%else%}11{%endif%}">{{p.topic}}</th></tr>
+   {%endifchanged%}
+  {%endif%}
+  <tr>
+   <td><a href="/patch/{{p.id}}/">{{p.name}}</a></td>
+   <td>{{p.id}}</td>
+   {%if user.is_authenticated %}
+    <td><a href="/{{p.cf_id}}/"><span class="label label-{{p.cf_status|commitfeststatuslabel}}">{{p.cf_name}}</span></a></td>
+   {%endif%}
+   <td><span class="label label-{{p.status|patchstatuslabel}}">{{p.status|patchstatusstring}}</span></td>
+   <td>{%if p.targetversion%}<span class="label label-default">{{p.targetversion}}</span>{%endif%}</td>
+   <td class="cfbot-summary">
+    {%with p.cfbot_results as cfb%}
+     {%if not cfb %}
+      <span class="label label-default">Not processed</span>
+     {%elif p.needs_rebase_since %}
+      <a href="{{cfb.apply_url}}" title="View git apply logs. Needs rebase since {{p.needs_rebase_since|timesince}}. {%if p.failing_since and p.failing_since != p.needs_rebase_since %}Failing since {{p.failing_since|timesince}}.{%endif%}">
+       <span class="label label-warning">Needs rebase!</span>
+      </a>
+     {%else%}
+      <a href="https://github.com/postgresql-cfbot/postgresql/compare/cf/{{p.id}}~1...cf/{{p.id}}" title="View last patch set on GitHub"><img class="github-logo" src="/media/commitfest/github-mark.svg"/></a>
+      <a href="https://cirrus-ci.com/github/postgresql-cfbot/postgresql/cf%2F{{p.id}}"
+         title="View CI history. {%if p.failing_since%}Failing since {{p.failing_since|timesince}}. {%endif%}{%if cfb.failed_task_names %}Failed jobs: {{cfb.failed_task_names}}{%endif%}">
+       {%if cfb.failed > 0 or cfb.branch_status == 'failed' or cfb.branch_status == 'timeout' %}
+        <img src="/media/commitfest/new_failure.svg"/>
+       {%elif cfb.completed < cfb.total  %}
+        <img src="/media/commitfest/running.svg"/>
+       {%else%}
+        <img src="/media/commitfest/new_success.svg"/>
+       {%endif%}
+       <span class="run-counters">
+        {{cfb.completed}}/{{cfb.total}}
+       </span>
+      </a>
+     {%endif%}
+     </td>
+     <td>
+      {%if cfb and cfb.all_additions is not none %}
+       <span class="additions">+{{ cfb.all_additions }}</span><span class="deletions">&#8722;{{ cfb.all_deletions }}</span>
+      {%endif%}
+     </td>
+    {%endwith%}
+    <td>{{p.author_names|default:''}}</td>
+    <td>{{p.reviewer_names|default:''}}</td>
+    <td>{{p.committer|default:''}}</td>
+    <td>{{p.num_cfs}}</td>
+    <td style="white-space: nowrap;" title="{{p.lastmail}}">{%if p.lastmail %}{{p.lastmail|timesince}} ago{%endif%}</td>
+    {%if user.is_staff%}
+     <td style="white-space: nowrap;"><input type="checkbox" class="sender_checkbox" id="send_authors_{{p.id}}">Author<br/><input type="checkbox" class="sender_checkbox" id="send_reviewers_{{p.id}}">Reviewer</td>
+    {%endif%}
+   </tr>
+   {%if forloop.last%}
+    </tbody>
+    </table>
+   {%endif%}
+ {%endfor%}
 {%endblock%}

pgcommitfest/commitfest/templatetags/commitfest.py
@@ -3,9 +3,23 @@ from django.template.defaultfilters import stringfilter
 
 from uuid import uuid4
 
-from pgcommitfest.commitfest.models import PatchOnCommitFest
+from pgcommitfest.commitfest.models import CommitFest, PatchOnCommitFest
 
 register = template.Library()
+
+
+@register.filter(name="commitfeststatusstring")
+@stringfilter
+def commitfeststatusstring(value):
+    i = int(value)
+    return [v for k, v in CommitFest._STATUS_CHOICES if k == i][0]
+
+
+@register.filter(name="commitfeststatuslabel")
+@stringfilter
+def commitfeststatuslabel(value):
+    i = int(value)
+    return [v for k, v in CommitFest._STATUS_LABELS if k == i][0]
 
 
 @register.filter(name="patchstatusstring")

commit 78bb20242f6a5729c36e8266623a6ee310e9f993

Fix redirects to login page when sorting by yourself
Broken by #51

pgcommitfest/commitfest/views.py
@@ -64,7 +64,7 @@ def home(request):
         patch_list = patchlist(request, cf)
 
     if patch_list.redirect:
-        return HttpResponseRedirect("/")
+        return patch_list.redirect
 
     return render(
         request,
@@ -222,8 +222,13 @@ def patchlist(request, cf, personalized=False):
         elif request.GET["author"] == "-3":
             # Checking for "yourself" requires the user to be logged in!
             if not request.user.is_authenticated:
-                return HttpResponseRedirect(
-                    "%s?next=%s" % (settings.LOGIN_URL, request.path)
+                return PatchList(
+                    patches=[],
+                    has_filter=False,
+                    sortkey=0,
+                    redirect=HttpResponseRedirect(
+                        "%s?next=%s" % (settings.LOGIN_URL, request.path)
+                    ),
                 )
             whereclauses.append(
                 "EXISTS (SELECT 1 FROM commitfest_patch_authors cpa WHERE cpa.patch_id=p.id AND cpa.user_id=%(self)s)"
@@ -247,8 +252,13 @@ def patchlist(request, cf, personalized=False):
         elif request.GET["reviewer"] == "-3":
             # Checking for "yourself" requires the user to be logged in!
             if not request.user.is_authenticated:
-                return HttpResponseRedirect(
-                    "%s?next=%s" % (settings.LOGIN_URL, request.path)
+                return PatchList(
+                    patches=[],
+                    has_filter=False,
+                    sortkey=0,
+                    redirect=HttpResponseRedirect(
+                        "%s?next=%s" % (settings.LOGIN_URL, request.path)
+                    ),
                 )
             whereclauses.append(
                 "EXISTS (SELECT 1 FROM commitfest_patch_reviewers cpr WHERE cpr.patch_id=p.id AND cpr.user_id=%(self)s)"
@@ -401,7 +411,12 @@ def patchlist(request, cf, personalized=False):
 
     if not has_filter and sortkey == 0 and request.GET:
         # Redirect to get rid of the ugly url
-        return PatchList(patches=[], has_filter=False, sortkey=0, redirect=True)
+        return PatchList(
+            patches=[],
+            has_filter=False,
+            sortkey=0,
+            redirect=HttpResponseRedirect(request.path),
+        )
 
     if whereclauses:
         where_str = "({0})".format(") AND (".join(whereclauses))
@@ -474,7 +489,7 @@ def commitfest(request, cfid):
 
     patch_list = patchlist(request, cf)
     if patch_list.redirect:
-        return HttpResponseRedirect(f"/{cf.id}/")
+        return patch_list.redirect
 
     # Generate patch status summary.
     curs = connection.cursor()