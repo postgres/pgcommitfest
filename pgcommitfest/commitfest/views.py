75b5fb467ae6df0dcb4f174441ecfab7ff50f328
Akshat Jaimini <destrex271@gmail.com>
2025-03-14 01:47:47 +0530

    added returned with feedback to not in filter

--- pgcommitfest/commitfest/views.py
+++ pgcommitfest/commitfest/views.py
@@ -95,7 +95,7 @@ def me(request):
         INNER JOIN commitfest_patch p ON p.id = poc.patch_id
         INNER JOIN commitfest_patchstatus ps ON ps.status=poc.status
         WHERE
-            ps.status NOT IN ( %(patch_stat_committed)s, %(patch_stat_rejected)s, %(patch_stat_withdrawn)s, %(patch_stat_nextcf)s )
+            ps.status NOT IN ( %(patch_stat_committed)s, %(patch_stat_rejected)s, %(patch_stat_withdrawn)s, %(patch_stat_nextcf)s, %(patch_stat_retwfdb)s )
         AND (
             EXISTS (
                 SELECT 1 FROM commitfest_patch_reviewers cpr WHERE cpr.patch_id=p.id AND cpr.user_id=%(user_id)s
@@ -111,6 +111,7 @@ def me(request):
             "patch_stat_rejected": PatchOnCommitFest.STATUS_REJECTED,
             "patch_stat_withdrawn": PatchOnCommitFest.STATUS_WITHDRAWN,
             "patch_stat_nextcf": PatchOnCommitFest.STATUS_NEXT,
+            "patch_stat_retwfdb": PatchOnCommitFest.STATUS_RETURNED,
         },
     )
     statussummary = curs.fetchall()
