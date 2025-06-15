8ce77886ae9c6847e80eadb10fed0ba4badb0d79
Jelte Fennema-Nio <postgres@jeltef.nl>
2025-06-15 21:57:39 +0200

    Introduce Drafts page and automatic CF creation (#70)
    This introduce a new type of CommitFest a "Draft" CommitFest. This
    CommitFest is never "In Progress", but it can be open. It exists for a
    year. It opens when the final regular CommitFest of the year becomes "In
    Progress" and stays open for exactly a year. It never becomes "In
    Progress" itself.
    
    Adding a second type of CommitFest also needed a redesign of quite a few
    things, like the homepage. Also management of the CommitFests needed to
    be made a bit easier, so admins don't forget to close/create Draft
    CommitFests. So now, closing/opening/creating CommitFests is done
    automatically when the time is right for that. A help page is also
    introduced to explain the CommitFest app.
    
    The naming of CommitFests has been changed too. Since we now have a
    Draft CF every year that needs a name, it seemed reasonable to align the
    names of the other CFs with that too. So each PG release cycle now has 5
    regular commitfests that are called:
    
    - PG18-1
    - PG18-2
    - PG18-3
    - PG18-4
    - PG18-Final
    
    And a single Draft CommitFest, called:
    
    - PG18-Draft
    
    Finally, it also adds a small initial API endpoint for the CFBot, to
    request the commitfests that need CI runs. Future PRs will extend this
    API surface to also include/allow requesting the actual patches that CI
    should run on.
    
    Fixes #25
    Fixes #65
    
    ---------
    
    Co-authored-by: David G. Johnston <david.g.johnston@gmail.com>

--- pgcommitfest/urls.py
+++ pgcommitfest/urls.py
@@ -3,6 +3,7 @@ from django.urls import re_path
 
 import pgcommitfest.auth
 import pgcommitfest.commitfest.ajax as ajax
+import pgcommitfest.commitfest.apiv1 as apiv1
 import pgcommitfest.commitfest.lookups as lookups
 import pgcommitfest.commitfest.reports as reports
 import pgcommitfest.commitfest.views as views
@@ -15,6 +16,9 @@ admin.autodiscover()
 
 urlpatterns = [
     re_path(r"^$", views.home),
+    re_path(r"^api/v1/commitfests/needs_ci$", apiv1.commitfestst_that_need_ci),
+    re_path(r"^help/$", views.help),
+    re_path(r"^commitfest_history/$", views.commitfest_history),
     re_path(r"^me/$", views.me),
     re_path(r"^archive/$", views.archive),
     re_path(r"^activity(?P<rss>\.rss)?/", views.activity),
@@ -26,9 +30,8 @@ urlpatterns = [
     re_path(r"^patch/(\d+)/edit/$", views.patchform),
     re_path(r"^(\d+)/new/$", views.newpatch),
     re_path(r"^patch/(\d+)/status/(review|author|committer)/$", views.status),
-    re_path(
-        r"^patch/(\d+)/close/(reject|withdrawn|feedback|committed|next)/$", views.close
-    ),
+    re_path(r"^patch/(\d+)/close/(reject|withdrawn|feedback|committed)/$", views.close),
+    re_path(r"^patch/(\d+)/move/$", views.move),
     re_path(r"^patch/(\d+)/reviewer/(become|remove)/$", views.reviewer),
     re_path(r"^patch/(\d+)/committer/(become|remove)/$", views.committer),
     re_path(r"^patch/(\d+)/(un)?subscribe/$", views.subscribe),
