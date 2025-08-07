22f65991de509586eea5922bcc6298895a4574e0
Jelte Fennema-Nio <postgres@jeltef.nl>
2025-08-07 23:19:24 +0200

    Format

--- media/commitfest/js/commitfest.js
+++ media/commitfest/js/commitfest.js
@@ -378,10 +378,12 @@ git checkout commitfest/cf/${patchId}
 /* Build our button callbacks */
 $(document).ready(() => {
     // Initialize Bootstrap tooltips
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl);
-    });
+    const tooltipTriggerList = [].slice.call(
+        document.querySelectorAll('[data-bs-toggle="tooltip"]'),
+    );
+    const tooltipList = tooltipTriggerList.map(
+        (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl),
+    );
 
     $("button.attachThreadButton").each((i, o) => {
         const b = $(o);
