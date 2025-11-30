a15be03e32e1523328fb13f239888df5cf3b0023
Jelte Fennema-Nio <postgres@jeltef.nl>
2025-11-30 23:39:31 +0100

    Don't allow global search of users without login anymore
    Apparently login requirement for the user lookup was added to make
    scraping harder. It doesn't seem like the most interesting data
    honestly, but this adds the requirement back for global user search.
    
    Instead it will allow anonymous searching of users involved in a certain
    commitfest. Those you could scrape from the commitfest page anyway.
    
    Finally this adds a logged in requirement for the global search page.
    Otherwise people will run into the same problem there as they did on the
    commitfest page previously: the search box would not autocomplete
    without giving feedback as to why to the user. Since the global search
    page is only really useful for power users, having it require a login
    seems fine.

--- pgcommitfest/commitfest/lookups.py
+++ pgcommitfest/commitfest/lookups.py
@@ -1,21 +1,39 @@
 from django.contrib.auth.models import User
 from django.db.models import Q
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseForbidden
 
 import json
 
 
 def userlookup(request):
     query = request.GET.get("query", None)
+    cf = request.GET.get("cf", None)
+
     if not query:
         raise Http404()
 
+    # Start with base filters for active users matching the query
     users = User.objects.filter(
         Q(is_active=True),
         Q(username__icontains=query)
         | Q(first_name__icontains=query)
         | Q(last_name__icontains=query),
     )
+
+    # If no commitfest filter is provided, require login
+    if not cf:
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden(
+                "Login required when not filtering by commitfest"
+            )
+    else:
+        # Filter users to only those who have participated in the specified commitfest
+        # This includes authors, reviewers, and committers of patches in that commitfest
+        users = users.filter(
+            Q(patch_author__commitfests__id=cf)
+            | Q(patch_reviewer__commitfests__id=cf)
+            | Q(committer__patch__commitfests__id=cf)
+        ).distinct()
 
     return HttpResponse(
         json.dumps(
